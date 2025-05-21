3131acd4e36463b2306d0c476d136e5bf8c0b388
luisjro16 <luis_jeronimo111@outlook.com>
2025-05-21 14:43:42 -0300

    Adicionar front-end

---
+++ front-end/src/app/index.tsx
@@ -0,0 +1,9 @@
+import {View, Text} from 'react-native'
+
+export default function App() {
+    return (
+        <View>
+            <Text style={{color:'red', fontSize:24}}>Hello World</Text>
+        </View>
+    )
+}
